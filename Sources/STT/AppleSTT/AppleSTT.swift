5d5304a722b4499b755e737fcf7ac731558574b1
Tomas Green <tomas.green@gmail.com>
2021-10-25 14:00:22 +0200

    Documenting code and refactoring some logic and variables. Breaks previous implementations by the new variable names

--- Sources/STT/AppleSTT/AppleSTT.swift
+++ Sources/STT/AppleSTT/AppleSTT.swift
@@ -3,19 +3,27 @@ import AVKit
 import Speech
 import Intents
 import Combine
-import STT
 import FFTPublisher
 import AudioSwitchboard
 
+/// AppleSTT Errors
 public enum AppleSTTError : Error {
+    /// User denied use of micophone
     case microphonePermissionsDenied
+    /// User denied use of speech recognizer
     case speechRecognizerPermissionsDenied
+    /// Unable to start the speech recognition without a good reason
     case unableToStartSpeechRecognition
+    /// The service is unavailable
     case unavailable
+    /// Trying to use an unsupported locale
     case unsupportedLocale
 }
 
+/// The AppleSTT is a STTService implementation using the Apple `SFSpeechRecognizer` framework
 public class AppleSTT: STTService, ObservableObject {
+    /// Indicates whether or not the service is available
+    /// - Note: Setting the availability to false will trigger change of status and immediately stops the current recognition session
     public private(set) var available:Bool = true {
         didSet {
             if available == false {
@@ -26,37 +34,63 @@ public class AppleSTT: STTService, ObservableObject {
             }
         }
     }
+    /// The current locale used to recgonize speech.
+    /// - Note: changing the locale will reset the current recognitions session, if one is ongoing
     public var locale: Locale = Locale.current {
         didSet { settingsUpdated() }
     }
+    /// Determines the maximum duration of slience allowed. Once triggered the instance will terminate the recognition session and publish the final restuls.
+    /// - Note: The threshold will only trigger after the recognizer has identified speech, if the user is silent the threshold will be ignored.
     public var maxSilence:TimeInterval = 2
+    /// Used to increase accuracy of the speech recognizer
     public var contextualStrings: [String] = [] {
         didSet { settingsUpdated() }
     }
+    /// Used to set the mode of the speech recognizer.
     public var mode: STTMode = .unspecified {
         didSet { settingsUpdated() }
     }
+    /// Private subjects used to publish results
     private let resultSubject: STTRecognitionSubject = .init()
+    /// Private subjects used to publish status
     private let statusSubject: STTStatusSubject = .init()
+    /// Private subjects used to publish errors
     private let errorSubject: STTErrorSubject = .init()
+    
+    /// Publishes complete or intermittently incomplete recognition results
     public let resultPublisher: STTRecognitionPublisher
+    /// Publishes the current status of the service
     public let statusPublisher: STTStatusPublisher
+    /// Publishes errors occoring
     public let errorPublisher: STTErrorPublisher
+    /// The current status of the service
     private var status:STTStatus = .idle {
         didSet {
             statusSubject.send(status)
         }
     }
+    /// Cancellable store
     private var cancellables = Set<AnyCancellable>()
+    /// Specific cancellable for the audio switchboard cancellation callback
     private var switchboardCancellable:AnyCancellable?
+    /// Optional fft publishing meter values from the microhpone
     private weak var fft:FFTPublisher? = nil
-    private let audioSwitchBoard:AudioSwitchboard
+    /// AudioSwitchboard used to manage audio ownership
+    private let audioSwitchboard:AudioSwitchboard
+    /// The bus used to tap the microphone
     private let bus:AVAudioNodeBus = 0
+    /// The current recognizer
     private var recognizer:SFSpeechRecognizer? = nil
+    /// The current recognizing request
     private var recognitionRequest:SFSpeechAudioBufferRecognitionRequest?
+    /// The current recognition task
     private var recognitionTask: SFSpeechRecognitionTask?
+    /// The most recent result
     private var currentResult:STTResult?
+    /// A timer used gother with the maxSilence to determine whether or not the user is done recording.
     private var clearTimer:Timer?
+    /// Starts a timer that monitors the users precense, and determines when to stop recording.
+    /// - Note: this function will not be called if the mode is set to dictation. Once the timer has been triggerd it will be restarted is the mode is set to `dictaion` or `unspecified`.
     private func startTimer() {
         clearTimer?.invalidate()
         clearTimer = nil
@@ -75,30 +109,35 @@ public class AppleSTT: STTService, ObservableObject {
             }
         })
     }
+    /// Internal stop function that releases instances, stops timers, releases audio ownershop etc.
     private func internalStop() {
         clearTimer?.invalidate()
         clearTimer = nil
         recognitionRequest?.endAudio()
-        audioSwitchBoard.stop(owner: "AppleSTT")
+        audioSwitchboard.stop(owner: "AppleSTT")
         recognitionRequest = nil
         recognitionTask = nil
         currentResult = nil
         fft?.end()
     }
+    /// Used to restart the service when settings are changed. Only triggers a restart if currently recording
     private func settingsUpdated() {
         if self.status == .recording {
             self.restart()
         }
     }
+    /// Indicates whether or not the use of Microphone and SFSpeechRecognizer has been accepted
     private var permissionsResolved:Bool {
         AVAudioSession.sharedInstance().recordPermission == .granted && SFSpeechRecognizer.authorizationStatus() == .authorized
     }
+    /// Restarts the service if available.
     private func restart() {
         internalStop()
         if !available {
             return
         }
         status = .preparing
+        /// Finalizes the start/restart and starts recording if possible.
         func finalize() {
             do {
                 try self.startRecording()
@@ -107,6 +146,7 @@ public class AppleSTT: STTService, ObservableObject {
                 self.errorSubject.send(AppleSTTError.microphonePermissionsDenied)
             }
         }
+        /// Resolves all permissions to the microphone and SFSpeechRecognizer
         func resolveAccess() {
             let audioSession = AVAudioSession.sharedInstance()
             if audioSession.recordPermission == .denied {
@@ -150,16 +190,17 @@ public class AppleSTT: STTService, ObservableObject {
             resolveAccess()
         }
     }
+    /// Start a new recording
     private func startRecording() throws {
         guard let recognizer = SFSpeechRecognizer(locale: locale) else {
             self.status = .idle
             self.errorSubject.send(AppleSTTError.unsupportedLocale)
             return
         }
-        switchboardCancellable = audioSwitchBoard.claim(owner: "AppleSTT").sink { [weak self] in
+        switchboardCancellable = audioSwitchboard.claim(owner: "AppleSTT").sink { [weak self] in
             self?.stop()
         }
-        let audioEngine = audioSwitchBoard.audioEngine
+        let audioEngine = audioSwitchboard.audioEngine
         self.recognizer = recognizer
         let inputNode = audioEngine.inputNode
         let inputNodeFormat = inputNode.inputFormat(forBus: bus)
@@ -179,7 +220,6 @@ public class AppleSTT: STTService, ObservableObject {
         }
         
         recognitionRequest.contextualStrings = contextualStrings
-        debugPrint(recognitionRequest.contextualStrings)
         recognizer.recognitionTask(with: recognitionRequest) { [weak self] result, error in
             guard let this = self else {
                 return
@@ -201,7 +241,7 @@ public class AppleSTT: STTService, ObservableObject {
             }
             let parts = result.bestTranscription.segments.map { STTResult.Segment.init(string: $0.substring, confidence: Double($0.confidence)) }
             let r = STTResult(result.bestTranscription.formattedString, segments: parts,locale:this.locale, final: result.isFinal)
-            if this.currentResult?.string == r.string && this.currentResult?.confidence == r.confidence && this.currentResult?.final == r.final  {
+            if this.currentResult?.string == r.string && this.currentResult?.confidence == r.confidence && this.currentResult?.isFinal == r.isFinal  {
                 return
             }
             this.currentResult = r
@@ -220,20 +260,29 @@ public class AppleSTT: STTService, ObservableObject {
         }
         let rate = Float(inputNodeFormat.sampleRate)
         inputNode.installTap(onBus: bus, bufferSize: 1024, format: inputNodeFormat) { [weak self] (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            /// Reducing the frameLeth to 512 makes the FFT considerably faster.
+            /// It's a something of a hack since apple doens't allow buffersize below in the installTap method.
+            /// There is a risk that this function will cause an error at some point.
             buffer.frameLength = 512
             self?.recognitionRequest?.append(buffer)
             self?.fft?.consume(buffer: buffer.audioBufferList, frames: buffer.frameLength, rate: rate)
         }
-        try? audioSwitchBoard.start(owner: "AppleSTT")
+        try? audioSwitchboard.start(owner: "AppleSTT")
         status = .recording
     }
-    public init(audioSwitchBoard: AudioSwitchboard, fft:FFTPublisher? = nil, maxSilence:TimeInterval = 2) {
+    
+    /// initializes a new AppleSTT service instance
+    /// - Parameters:
+    ///   - audioSwitchboard:  AudioSwitchboard used to manage audio ownership
+    ///   - fft: Optional fft publishing meter values from the microhpone
+    ///   - maxSilence: Determines the maximum duration of slience allowed. Once triggered the instance will terminate the recognition session and publish the final restuls.
+    public init(audioSwitchboard: AudioSwitchboard, fft:FFTPublisher? = nil, maxSilence:TimeInterval = 2) {
         self.resultPublisher = resultSubject.eraseToAnyPublisher()
         self.statusPublisher = statusSubject.eraseToAnyPublisher()
         self.errorPublisher = errorSubject.eraseToAnyPublisher()
         self.fft = fft
         self.maxSilence = maxSilence
-        self.audioSwitchBoard = audioSwitchBoard
+        self.audioSwitchboard = audioSwitchBoard
         self.available = audioSwitchBoard.availableServices.contains(.record)
         audioSwitchBoard.$availableServices.sink { [weak self] services in
             if services.contains(.record) == false {
@@ -244,6 +293,7 @@ public class AppleSTT: STTService, ObservableObject {
             }
         }.store(in: &cancellables)
     }
+    /// Start the service if available
     public func start() {
         if !self.available {
             self.errorSubject.send(AppleSTTError.unavailable)
@@ -255,6 +305,7 @@ public class AppleSTT: STTService, ObservableObject {
         self.restart()
     }
     
+    /// Stops the service immediately if started. Publishes a final result based on the currently recognized string.
     public func stop() {
         guard status == .recording || status == .preparing else {
             return
@@ -264,6 +315,7 @@ public class AppleSTT: STTService, ObservableObject {
         internalStop()
     }
     
+    /// Stops the service but waits for the recognizer to publish it's final result.
     public func done() {
         guard status == .recording || status == .preparing else {
             return
@@ -275,6 +327,13 @@ public class AppleSTT: STTService, ObservableObject {
         }
         recognitionTask?.finish()
         internalStop()
+    }
+    /// Returns whether or not SFSpeechRecognizer supports the given locale
+    /// - Parameter locale: the locale to compare with
+    /// - Returns: true if available false if not
+    public static func hasSupportFor(locale:Locale) -> Bool {
+        let identifier = locale.identifier.replacingOccurrences(of: "_", with: "-")
+        return SFSpeechRecognizer.supportedLocales().contains { $0.identifier == identifier }
     }
 }
 
