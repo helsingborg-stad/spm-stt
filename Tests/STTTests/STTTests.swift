63b00c28010126ab912283b36f03fcb5ce5c6d46
Tomas Green <tomas.green@gmail.com>
2021-10-25 14:14:47 +0200

    Update STTTests.swift

--- Tests/STTTests/STTTests.swift
+++ Tests/STTTests/STTTests.swift
@@ -1,19 +1,9 @@
 import XCTest
 @testable import STT
 
-struct TestObj : Codable {
-    var test:String?
-}
-
 final class STTTests: XCTestCase {
     func testSupportsLocale() {
         XCTAssert(AppleSTT.hasSupportFor(locale: Locale(identifier: "en_US")))
         XCTAssertFalse(AppleSTT.hasSupportFor(locale: Locale(identifier: "agq_CM")))
-    }
-    func testJSON () {
-        let e = JSONEncoder()
-        //e.outputFormatting = .
-        let data = try! e.encode(TestObj(test: nil))
-        debugPrint(String(data: data, encoding: .utf8))
     }
 }
